fe6d91d537c9f8950206ebb7d06ae45dceb590c5
just10minutes@gmail.com <just10minutes@gmail.com>
2019-11-28 22:17:50 +0200

    Initial

---
+++ pages/recipes/_index.vue
@@ -0,0 +1,29 @@
+<template>
+  <section class="wrapper">
+    <Sections v-if="wpData" :data="recipes" />
+  </section>
+</template>
+
+<script>
+import wpData from '@vue-wordpress/core/mixins/wpData'
+import { FetchHookTypes } from '@vue-wordpress/core/types'
+
+export default {
+  name: 'MyCustomPage',
+  mixins: [
+    wpData(
+      {
+        slug: 'hello-world',
+        type: 'posts',
+        dataName: 'recipes'
+      },
+      FetchHookTypes.AsyncData
+    )
+  ],
+  mounted() {
+    // There we have access to 'sample-post-page' response!
+    // eslint-disable-next-line no-console
+    console.log(this.recipes)
+  }
+}
+</script>
